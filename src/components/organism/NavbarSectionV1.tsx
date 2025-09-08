43ca2e2f8de44aea7f7e6ffa9a83407c9df1d01a
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-09-08 12:18:12 +0700

    chore : update NavbarSectionV1

--- src/components/organism/NavbarSectionV1.tsx
+++ src/components/organism/NavbarSectionV1.tsx
@@ -12,7 +12,7 @@ export default function NavbarSection() {
     return (
         // <nav className="fixed top-0 left-0 z-50 w-full bg-white/80 backdrop-blur-md">
         <nav className="fixed top-0 left-0 z-50 w-full bg-transparent">
-            <div className={`container mx-auto flex h-16 items-center justify-end px-4 md:px-8 gap-4 animate-fade-in`}>
+            <div className={`mx-auto flex h-16 items-center justify-end px-4 md:px-8 gap-4 animate-fade-in`}>
                 <div onClick={toggleOpen} className="p-2 flex items-center bg-muted rounded-md gap-16 cursor-pointer">
                     <p className="text-sm text-muted-foreground">Search on Page...</p>
                     <Badge variant="default">CtrlK</Badge>
