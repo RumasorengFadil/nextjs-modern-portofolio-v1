46cb27a641610d57bad63fdca6230d3f293db44c
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-10-02 19:06:22 +0900

    chore : update CtaSectionV1 contact button

--- src/components/organism/CtaSectionV1.tsx
+++ src/components/organism/CtaSectionV1.tsx
@@ -1,3 +1,4 @@
+import { siteConfig } from "@/config/site"
 import { Button } from "../ui/button"
 import Link from "next/link"
 
@@ -7,7 +8,7 @@ export default function CtaSection() {
             <h2 className="text-4xl font-bold">{"Let's Work Together"}</h2>
             <p className="mt-4 text-muted-foreground">Interested in collaborating or need project assistance? Please contact me.</p>
             <div className="mt-6">
-                <Button asChild size="lg"><Link href="/contact">Contact Me</Link></Button>
+                <Button asChild size="lg"><Link href={`mailto:${siteConfig.email}`}>Contact Me</Link></Button>
             </div>
         </section>
     )
