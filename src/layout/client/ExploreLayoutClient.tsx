3ad17004fe7376e44f923d08b015529f6ca7fdce
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-10-13 15:12:22 +0900

    chore : update ExploreLayoutClient.tsx

--- src/layout/client/ExploreLayoutClient.tsx
+++ src/layout/client/ExploreLayoutClient.tsx
@@ -3,6 +3,7 @@ import ApplicationLogo from "@/components/ApplicationLogo";
 import { useThemeCookie } from "@/hooks/use-theme-cookie";
 import { useAuthStore } from "@/store/use-auth-store";
 import { Sun } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 
@@ -14,8 +15,8 @@ export default function ExploreLayoutClient({ children }: { children: ReactNode 
         <div className="min-h-screen font-sans antialiased">
             <header className="border-b z-50 bg-background">
                 <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-                    <Link href="/explore" className="flex space-x-4 items-center text-xl font-bold tracking-tight">
-                        <ApplicationLogo className="w-12 h-12" />
+                    <Link href="/explore" className="flex space-x-2 items-center text-xl font-bold tracking-tight">
+                        <Image src="/images/app/logo-no-text.png" width={32} height={32} alt="fadilrumasoreng" />
                         <span>Rumasoreng Blog</span>
                     </Link>
                     <nav className="flex items-center space-x-4 text-sm">
@@ -35,10 +36,10 @@ export default function ExploreLayoutClient({ children }: { children: ReactNode 
             <section className="py-16 border-b">
                 <div className="container mx-auto px-4 text-center">
                     <h1 className="text-4xl font-bold leading-tight mb-4 tracking-tight">
-                        Wawasan & Cerita dari Fadil Rumasoreng
+                        Blog by Fadil Rumasoreng
                     </h1>
                     <p className="text-lg text-gray-600 max-w-2xl mx-auto">
-                        Saya menulis tentang pengalaman membangun aplikasi, eksperimen teknologi, dan ide-ide seputar web development. Semua ini saya bagikan untuk membantu developer lain belajar lebih cepat dan tetap up-to-date.
+                        Saya menulis tentang pengalaman saya membangun aplikasi, eksperimen teknologi, dan ide-ide seputar web development. Semua ini saya bagikan atau saya tulis murni dari pengalaman yang saya dapatkan ketika mengembangkan perangkat lunak.
                     </p>
                 </div>
             </section>
