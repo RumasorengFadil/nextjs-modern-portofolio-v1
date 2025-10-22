610bf53c9c0310af130e41fce804fee30fd23d75
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-10-22 23:12:22 +0900

    chore : update HeroSectionV2.tsx

--- src/components/organism/HeroSectionV2.tsx
+++ src/components/organism/HeroSectionV2.tsx
@@ -14,7 +14,7 @@ export default function HeroSection() {
                 <h1 className="py-3.5 px-0.5 z-10 text-4xl font-bold md:text-8xl text-transparent animate-title bg-black dark:bg-white sm:text-5xl duration-1000 whitespace-nowrap bg-clip-text">RUMASORENG</h1>
                 <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
-                <p className="mt-4 text-muted-foreground animate-fade-in">I am a Fullstack Developer who focuses on building modern web applications with high performance.</p>
+                <p className="mt-4 text-muted-foreground animate-fade-in">I’m a Fullstack Web Developer passionate about building scalable, elegant web solutions that solve real business problems.</p>
 
                 <div className="mt-8 flex justify-center gap-3 animate-fade-in">
                     <Button asChild size="lg"><Link href="/explore">See Blog</Link></Button>
